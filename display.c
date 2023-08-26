22c5d31b5dd3544958bf11a38e73fd04ed9fdc9a
Petr Vasilyev <petr123334>
2023-08-26 21:30:55 +0300

    initial commit

--- display.c
+++ display.c
@@ -1,8 +1,13 @@
 #include "display.h"
 #include "font.h"
 
+static void Display_command( struct DisplayGL * display, uint8_t command );
+static void Display_data( struct DisplayGL *display, uint8_t data );
+static void Display_setRamAddr( struct DisplayGL * display, uint8_t x0, uint8_t y0, uint8_t width, uint8_t height );
+
 /*
- * Display initialization.
+ * @brief Initialize display
+ *
  * Initialization is carried out in 3 stages:
  * GPIO initialization,
  * SPI/I2C initialization,
@@ -10,67 +15,132 @@
  * @param display - pointer to display struct
  * @return void
  */
-void Display_init(struct DisplayGL *display)
+void Display_init( struct DisplayGL * display )
 {
 	uint16_t i;
 
-	GPIO_clockInit();
+	GPIO_clockInit();	/* Enable GPIO */
 
-#if defined(DISPLAY_USE_SPI)
+#if defined(DISPLAY_USE_HW_4SPI)
 
-	GPIO_configPin(display->clkPinPort, display->clkPin, GPIO_OUTPUT_AF_PUSH_PULL);
-	GPIO_configPin(display->dataPinPort, display->dataPin, GPIO_OUTPUT_AF_PUSH_PULL);
+	GPIO_configPin(display->clkPinPort, display->clkPin, GPIO_OUTPUT_AF_PUSH_PULL);		/* Set CLK as alternate output */
+	GPIO_configPin(display->dataPinPort, display->dataPin, GPIO_OUTPUT_AF_PUSH_PULL);	/* Set DATA as alternate output */
 
-	SPI_clockEn();
+	SPI_clockEn();	/* Enable SPI */
 
-	SPI_masterInit(display->spi);
+	SPI_masterInit(display->spi);	/* Init SPI as master */
 
 #elif defined(DISPLAY_USE_SW_SPI)
 
-	GPIO_configPin(display->clkPinPort, display->clkPin, GPIO_OUTPUT_PUSH_PULL);
-	GPIO_configPin(display->dataPinPort, display->dataPin, GPIO_OUTPUT_PUSH_PULL);
+	GPIO_configPin(display->clkPinPort, display->clkPin, GPIO_OUTPUT_PUSH_PULL);	/* Set CLK as output */
+	GPIO_configPin(display->dataPinPort, display->dataPin, GPIO_OUTPUT_PUSH_PULL);	/* Set DATA as output */
 
 #endif
 
-	GPIO_configPin(display->csPinPort, display->csPin, GPIO_OUTPUT_PUSH_PULL);
+	GPIO_configPin(display->csPinPort, display->csPin, GPIO_OUTPUT_PUSH_PULL);	/* Set CS, DC, RES as output */
 	GPIO_configPin(display->dcPinPort, display->dcPin, GPIO_OUTPUT_PUSH_PULL);
 	GPIO_configPin(display->resPinPort, display->resPin, GPIO_OUTPUT_PUSH_PULL);
 
-	GPIO_setPin(display->csPinPort, display->csPin, 1);
-	GPIO_setPin(display->resPinPort, display->resPin, 0);
+	GPIO_setPin(display->csPinPort, display->csPin, 1);		/* Unselect display (CS = 1) */
+	GPIO_setPin(display->resPinPort, display->resPin, 0);	/* Reset display (RES = 0) */
 
-	for(i = 0; i < DISPLAY_BUFFER_SIZE; i++) /*clear display buffer from data-trash*/
-	{
-		display->displayBuffer[i] = 0;
-	}
+	for(i = 0; i < 1024; i++); 	/* Delay */
 
-	GPIO_setPin(display->resPinPort, display->resPin, 1);
+	GPIO_setPin(display->resPinPort, display->resPin, 1);	/* End reset (RES = 1) */
 
-	Display_command(display, 0xAE); /*Send control bytes*/
-	Display_command(display, 0xD5);
-	Display_command(display, 0x80);
-	Display_command(display, 0xA8);
-	Display_command(display, 0x3F);
-	Display_command(display, 0xD3);
-	Display_command(display, 0x00);
-	Display_command(display, 0x40);
-	Display_command(display, 0x8D);
-	Display_command(display, 0x14);
-	Display_command(display, 0x20);
-	Display_command(display, 0x00);
-	Display_command(display, 0xA1);
-	Display_command(display, 0xC8);
-	Display_command(display, 0xDA);
-	Display_command(display, 0x12);
-	Display_command(display, 0x81);
-	Display_command(display, 0x8F);
-	Display_command(display, 0xD9);
+	/* Send control bytes to the display */
+
+	/* Set command lock */
+	Display_command(display, 0xFD);
+	Display_data(display, 0x12);
+
+	/* Set command lock */
+	Display_command(display, 0xFD);
+	Display_data(display, 0xB1);
+
+	/* Turn display off */
+	Display_command(display, 0xAE);
+
+	/* Set display clock div */
+	Display_command(display, 0xB3);
+
+	/* 7:4 = oscillator frequency, 3:0 = CLK div ratio (A[3:0]+1 = 1..16) */
 	Display_command(display, 0xF1);
-	Display_command(display, 0xDB);
-	Display_command(display, 0x10);
-	Display_command(display, 0xA4);
+
+	/* Set display mux ratio */
+	Display_command(display, 0xCA);
+	Display_data(display, 0x7f);
+
+	/* Set display remap */
+	Display_command(display, 0xA0);
+	Display_data(display, 0x74);
+
+	/* Set display column */
+	Display_command(display, 0x15);
+	Display_data(display, 0x00);
+	Display_data(display, 0x7F);
+
+	/* Set display row*/
+	Display_command(display, 0x75);
+	Display_data(display, 0x00);
+	Display_data(display, 0x7F);
+
+	/* Set display start line */
+	Display_command(display, 0xA1);
+	Display_data(display, 0x00);
+
+	/* Set display offset */
+	Display_command(display, 0xA2);
+	Display_data(display, 0x00);
+
+	/* Set display controller GPIO */
+	Display_command(display, 0xB5);
+	Display_data(display, 0x00);
+
+	/* Display function select */
+	Display_command(display, 0xAB);
+	Display_data(display, 0x01);
+
+	/* Set display precharge */
+	Display_command(display, 0xB1);
+	Display_data(display, 0x32);
+
+	/* Set display VCOMH */
+	Display_command(display, 0xBE);
+	Display_data(display, 0x05);
+
+	/* Set display normal state (not inverted) */
 	Display_command(display, 0xA6);
+
+	/* Set display contrast */
+	Display_command(display, 0xC1);
+	Display_data(display, 0xC8);
+	Display_data(display, 0x80);
+	Display_data(display, 0xC8);
+
+	/* Set CONTRASTMSTR */
+	Display_command(display, 0xC7);
+	Display_data(display, 0x0F);
+
+	/* Set display VSL */
+	Display_command(display, 0xB4);
+	Display_data(display, 0xA0);
+	Display_data(display, 0xB5);
+	Display_data(display, 0x55);
+
+	/* Set display precharge2 */
+	Display_command(display, 0xB6);
+	Display_data(display, 0x01);
+
+	/* Turn display on */
 	Display_command(display, 0xAF);
+
+	Display_setBackColor(display, DISPLAY_DEFAULT_BACK_COLOR);	/* Default color settings */
+	Display_setDrawColor(display, DISPLAY_DEFAULT_DRAW_COLOR);
+
+	Display_setDrawMode(display, DISPLAY_DEFAULT_DRAW_MODE);
+
+	Display_clear(display);	/* Clear display RAM */
 }
 
 
@@ -80,23 +150,23 @@ void Display_init(struct DisplayGL *display)
  * @param command - command byte
  * @return - void
  */
-void Display_command(struct DisplayGL *display, uint8_t command)
+void Display_command( struct DisplayGL * display, uint8_t command )
 {
-	GPIO_setPin(display->dcPinPort, display->dcPin, 0);
+	GPIO_setPin(display->dcPinPort, display->dcPin, 0);	/* Set command-mode (DC = 0) */
 
-	GPIO_setPin(display->csPinPort, display->csPin, 0);
+	GPIO_setPin(display->csPinPort, display->csPin, 0);	/* Select display (CS = 0) */
 
-#if defined(DISPLAY_USE_SPI)
+#if defined(DISPLAY_USE_HW_4SPI)
 
-	SPI_sendByte(display->spi, command);
+	SPI_sendByte(display->spi, command );	/* Send command byte */
 
 #elif defined(DISPLAY_USE_SW_SPI)
 
-	swSpiWrite(display, command);
+	swSpiWrite( display, command );
 
 #endif
 
-	GPIO_setPin(display->csPinPort, display->csPin, 1);
+	GPIO_setPin(display->csPinPort, display->csPin, 1);	/* Unselect display */
 }
 
 
@@ -106,76 +176,50 @@ void Display_command(struct DisplayGL *display, uint8_t command)
  * @param data - data byte
  * @return - void
  */
-void Display_data(struct DisplayGL *display, uint8_t data)
+void Display_data( struct DisplayGL * display, uint8_t data )
 {
-	GPIO_setPin(display->dcPinPort, display->dcPin, 1);
+	GPIO_setPin(display->dcPinPort, display->dcPin, 1);	/* Set data-mode  (DC = 1) */
 
-	GPIO_setPin(display->csPinPort, display->csPin, 0);
+	GPIO_setPin(display->csPinPort, display->csPin, 0);	/* Select display (CS = 0) */
 
-#if defined(DISPLAY_USE_SPI)
+#if defined(DISPLAY_USE_HW_4SPI)
 
-	SPI_sendByte(display->spi, data);
+	SPI_sendByte(display->spi, data);	/* Send data byte */
 
 #elif defined(DISPLAY_USE_SW_SPI)
 
 	swSpiWrite(display, data);
 #endif
 
-	GPIO_setPin(display->csPinPort, display->csPin, 1);
+	GPIO_setPin(display->csPinPort, display->csPin, 1);	/* Unselect display */
 }
-
 
 /*
- * Send the contents of the buffer to the display
- * @param display - pointer to the display struct
- * @return - void
+ * Select display RAM address
+ * @param display - pointer to display struct
+ * @param x0
  */
-void Display_sendBuffer(struct DisplayGL *display)
+void Display_setRamAddr( struct DisplayGL * display, uint8_t x0, uint8_t y0, uint8_t width, uint8_t height )
 {
-	uint16_t i;
+	uint16_t x1, y1;
 
-	Display_command(display, 0x21);
-	Display_command(display, 0);
-	Display_command(display, 127);
+	if(x0 > DISPLAY_WIDTH || y0 > DISPLAY_HEIGHT) return;
 
-	Display_command(display, 0x22);
-	Display_command(display, 0);
-	Display_command(display, 7);
+	x1 = x0 + width - 1;
+	y1 = y0 + height - 1;
 
-	for(i = 0; i < DISPLAY_BUFFER_SIZE; i++)
-	{
-		Display_data(display, display->displayBuffer[i]);
-	}
+	if(x1 > DISPLAY_WIDTH || y1 > DISPLAY_HEIGHT) return;
+
+	Display_command(display, 0x15); /* set column */
+	Display_data(display, x0);
+	Display_data(display, x1);
+
+	Display_command(display, 0x75); /* set row*/
+	Display_data(display, y0);
+	Display_data(display, y1);
+
+	Display_command(display, 0x5C); /* enable display RAM output */
 }
-
-
-/*
- * clear the contents of display buffer.
- * The data already loaded to the display does not change
- * @param display - pointer to the display struct
- * @return - void
- */
-void Display_clearBuffer(struct DisplayGL *display)
-{
-	uint16_t i;
-
-	if(display->currentDrawColor == COLOR_WHITE)
-	{
-		for(i = 0; i < DISPLAY_BUFFER_SIZE; i++)
-		{
-			display->displayBuffer[i] = 0;
-		}
-	}
-	else if(display->currentDrawColor == COLOR_BLACK)
-	{
-		for(i = 0; i < DISPLAY_BUFFER_SIZE; i++)
-		{
-			display->displayBuffer[i] = 0xff;
-		}
-
-	}
-}
-
 
 /*
  * Clear data that loaded to the display. Also clears buffer
@@ -184,8 +228,27 @@ void Display_clearBuffer(struct DisplayGL *display)
  */
 void Display_clear(struct DisplayGL *display)
 {
-	Display_clearBuffer(display);
-	Display_sendBuffer(display);
+	uint16_t i;
+
+	Display_setRamAddr(display, 0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT);
+
+	for(i = 0; i < DISPLAY_RAM_SIZE; i++)
+	{
+		Display_data(display, DISPLAY_DEFAULT_BACK_COLOR);
+	}
+}
+
+void Display_fill( struct DisplayGL * display, uint16_t color )
+{
+	uint16_t i;
+
+	Display_setRamAddr(display, 0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT);
+
+	for(i = 0; i < DISPLAY_RAM_SIZE; i++)
+	{
+		Display_data(display, color >> 8);
+		Display_data(display, color & 0xFF);
+	}
 }
 
 
@@ -195,11 +258,20 @@ void Display_clear(struct DisplayGL *display)
  * @param color - the color that should be fixed
  * @return - void
  */
-void Display_setDrawColor(struct DisplayGL *display, DisplayColor_e color)
+void Display_setDrawColor(struct DisplayGL *display, uint16_t color)
 {
 	display->currentDrawColor = color;
 }
 
+void Display_setBackColor(struct DisplayGL *display, uint16_t color)
+{
+	display->currentBackColor = color;
+}
+
+void Display_setDrawMode(struct DisplayGL *display, uint8_t mode)
+{
+	display->drawMode = mode;
+}
 
 /*
  * Draw a pixel at the specified coordinates
@@ -208,84 +280,17 @@ void Display_setDrawColor(struct DisplayGL *display, DisplayColor_e color)
  * @param y - y-axis coordinate
  * @return - void
  */
-void Display_drawPixel(struct DisplayGL *display, uint8_t x, uint8_t y)
+void Display_drawPixel(struct DisplayGL *display, uint8_t x, uint8_t y, uint16_t color)
 {
 	if(x > DISPLAY_WIDTH - 1 || y > DISPLAY_HEIGHT - 1) return;
 
-	if(display->currentDrawColor == COLOR_WHITE)
-	{
-		display->displayBuffer[x + (y / 8) * DISPLAY_WIDTH] |= (1 << (y % 8));
-	}
-	else if(display->currentDrawColor == COLOR_BLACK)
-	{
-		display->displayBuffer[x + (y / 8) * DISPLAY_WIDTH] &= ~(1 << (y % 8));
-	}
+	Display_setRamAddr(display, x, y, 1, 1);
+
+	Display_data(display, color >> 8);
+	Display_data(display, color & 0x00FF );
 }
 
 
-/*
- * Clear a pixel at the specified coordinates
- * @param display - pointer to the display struct
- * @param x - x-axis coordinate
- * @param y - y-axis coordinate
- * @return - void
- */
-void Display_clearPixel(struct DisplayGL *display, uint8_t x, uint8_t y)
-{
-	if(x > DISPLAY_WIDTH - 1 || y > DISPLAY_HEIGHT - 1) return;
-
-	if(display->currentDrawColor == COLOR_BLACK)
-	{
-		display->displayBuffer[x + (y / 8) * DISPLAY_WIDTH] |= (1 << (y % 8));
-	}
-	else if(display->currentDrawColor == COLOR_WHITE)
-	{
-		display->displayBuffer[x + (y / 8) * DISPLAY_WIDTH] &= ~(1 << (y % 8));
-	}
-}
-
-
-/*
- * Fill the display with the current draw color
- * @param display - pointer to the display struct
- * @return - void
- */
-void Display_fill(struct DisplayGL *display)
-{
-	uint16_t i;
-
-	if(display->currentDrawColor == COLOR_WHITE)
-	{
-		for(i = 0; i < DISPLAY_BUFFER_SIZE; i++)
-		{
-			display->displayBuffer[i] = 0xff;
-		}
-	}
-	else if(display->currentDrawColor == COLOR_BLACK)
-	{
-		for(i = 0; i < DISPLAY_BUFFER_SIZE; i++)
-		{
-			display->displayBuffer[i] = 0x00;
-		}
-
-	}
-}
-
-
-/*
- * Invert colors of display buffer
- * The data already loaded to the display does not change
- * @param display - pointer to the display struct
- */
-void Display_invert(struct DisplayGL *display)
-{
-	uint16_t i;
-
-	for(i = 0; i < DISPLAY_BUFFER_SIZE; i++)
-	{
-		display->displayBuffer[i] = ~display->displayBuffer[i];
-	}
-}
 
 
 /*
@@ -296,11 +301,11 @@ void Display_invert(struct DisplayGL *display)
  * @param asciiChr - char to draw
  * @return - void
  */
-void Display_drawAsciiChar(struct DisplayGL *display, uint8_t str, uint8_t col, uint8_t asciiChr)
+void Display_drawAsciiChar(struct DisplayGL *display, uint8_t x, uint8_t y, uint8_t asciiChr)
 {
-	uint8_t i;
+	uint8_t i, j;
 
-	if(col > 20 || str > 7) return; /*checking for compliance with restrictions*/
+	if(x > DISPLAY_WIDTH || y > DISPLAY_HEIGHT) return; /*checking for compliance with restrictions*/
 
 	if((asciiChr >= 0x20) && (asciiChr <= 0x7f)) /*offset selection*/
 	{
@@ -320,20 +325,22 @@ void Display_drawAsciiChar(struct DisplayGL *display, uint8_t str, uint8_t col, 
 	 * it is recommended to rewrite it later
 	 */
 
-	if(display->currentDrawColor == COLOR_WHITE)
+
+	for(i = 0; i < 5; i++)
 	{
-		for(i = 0; i < 5; i ++)
+		for(j = 0; j < 8; j++)
 		{
-			display->displayBuffer[DISPLAY_WIDTH * str + col*6 + i] = font_5x8[asciiChr * 5 + i];
+			if(font_5x8[asciiChr * 5 + i] & (1 << j))
+			{
+				Display_drawPixel(display, x + i, y + j, display->currentDrawColor);
+			}
+			else if(!(font_5x8[asciiChr * 5 + i] & (1 << j)) && display->drawMode == DISPLAY_DRAW_MODE_OVERRIDE)
+			{
+				Display_drawPixel(display, x + i, y + j, display->currentBackColor);
+			}
 		}
 	}
-	else if(display->currentDrawColor == COLOR_BLACK)
-	{
-		for(i = 0; i < 5; i ++)
-		{
-			display->displayBuffer[DISPLAY_WIDTH * str + col + i] = ~font_5x8[asciiChr * 5 + i];
-		}
-	}
+
 }
 
 
@@ -341,24 +348,27 @@ void Display_drawAsciiChar(struct DisplayGL *display, uint8_t str, uint8_t col, 
  * Set display cursor to the specified position
  * @param display
  */
-void Display_setCursor(struct DisplayGL *display, uint8_t str, uint8_t col)
+void Display_setCursor(struct DisplayGL *display, uint8_t x, uint8_t y)
 {
-	if(col > 21 || str > 7) return;
+	if(x > DISPLAY_WIDTH || y > DISPLAY_HEIGHT) return;
 
-	display->cursorCol = col;
-	display->cursorStr = str;
+	display->cursorX = x;
+	display->cursorY = y;
 }
 
-void Display_printString(struct DisplayGL *display, uint8_t str[])
+#define DISPLAY_CURSOR_OFFSET_X	(uint8_t)(DISPLAY_FONT_WIDTH + 1)
+#define DISPLAY_CURSOR_OFFSET_Y	(uint8_t)(DISPLAY_FONT_HEIGHT + 1)
+
+void Display_printString(struct DisplayGL *display, char str[])
 {
 	uint8_t i = 0;
 
-	if(display->cursorStr > 7 || display->cursorCol > 127) return;
+	if(display->cursorX > DISPLAY_WIDTH || display->cursorY > DISPLAY_HEIGHT) return;
 
-	while(str[i] != 0 && display->cursorCol < 21)
+	while(str[i] != 0 && display->cursorX < DISPLAY_WIDTH)
 	{
-		Display_drawAsciiChar(display, display->cursorStr, display->cursorCol, str[i]);
-		display->cursorCol++;
+		Display_drawAsciiChar(display, display->cursorX, display->cursorY, str[i]);
+		display->cursorX += 6;
 		i++;
 	}
 
@@ -372,17 +382,16 @@ void Display_printNum(struct DisplayGL *display, int32_t num)
 
 	if(num == 0)
 	{
-		Display_drawAsciiChar(display, display->cursorStr, display->cursorCol, '0');
-		display->cursorCol++;
+		Display_drawAsciiChar(display, display->cursorX, display->cursorY, '0');
+		display->cursorX += DISPLAY_CURSOR_OFFSET_X;
 		return;
 	}
 
-	numBuff = num;
-
 	if(num < 0)
 	{
-		Display_drawAsciiChar(display, display->cursorStr, display->cursorCol, '-');
-		display->cursorCol++;
+		Display_drawAsciiChar(display, display->cursorX, display->cursorY, '-');
+		display->cursorX += DISPLAY_CURSOR_OFFSET_X;
+
 		num = num * -1;
 	}
 
@@ -394,44 +403,71 @@ void Display_printNum(struct DisplayGL *display, int32_t num)
 		numBuff /= 10;
 	}
 
-	while((numLength--) && display->cursorCol < 21)
+	while((numLength--) && display->cursorX < DISPLAY_WIDTH)
 	{
 		numBuff = (num / powl(10, numLength));
 		numChar = numBuff + '0';
-		num -= numBuff*powl(10, numLength);
+		num -= numBuff * powl(10, numLength);
 
-		Display_drawAsciiChar(display, display->cursorStr, display->cursorCol, numChar);
-		display->cursorCol++;
+		Display_drawAsciiChar(display, display->cursorX, display->cursorY, numChar);
+
+		display->cursorX += DISPLAY_CURSOR_OFFSET_X;
 	}
 }
 
 void Display_print(struct DisplayGL *display, const char *format, ...)
 {
-	uint8_t buf[21];
-
-	va_list arg;
-
-	va_start(arg, format);
-	//printf(buf, format, arg);
-	va_end(arg);
-
-	Display_printString(display, buf);
+	//TODO
 }
 
-void Display_clearString(struct DisplayGL *display, uint8_t str)
+void Display_drawFrame(struct DisplayGL *display, uint8_t x, uint8_t y, uint8_t width, uint8_t height)
 {
-	uint8_t i;
+	uint8_t i, j;
 
-	for(i = 0; i < DISPLAY_WIDTH; i++)
+	for(i = 0; i < height; i++)
 	{
-		display->displayBuffer[(str * DISPLAY_WIDTH) + i] = 0x00;
+		Display_drawPixel(display, x, y + i, display->currentDrawColor);
+	}
+
+	for(i = 1; i < width - 1; i++)
+	{
+		Display_drawPixel(display, x + i, y, display->currentDrawColor);
+
+		if(display->drawMode == DISPLAY_DRAW_MODE_OVERRIDE)
+		{
+			for(j = 1; j < height; j++)
+			{
+				Display_drawPixel(display, x + i, y + j, display->currentBackColor);
+			}
+		}
+
+		Display_drawPixel(display, x + i, y + height - 1, display->currentDrawColor);
+
+	}
+
+	for(i = 0; i < height; i++)
+	{
+		Display_drawPixel(display, x + width - 1, y + i, display->currentDrawColor);
 	}
 }
 
-void Display_setXbmMode(struct DisplayGL *display, DisplayXbmMode_e mode)
+
+void Display_drawBox(struct DisplayGL *display, uint8_t x, uint8_t y, uint8_t width, uint8_t height)
 {
-	display->xbmMode = mode;
+	uint8_t i, j;
+
+	if(x > DISPLAY_WIDTH || y > DISPLAY_HEIGHT) return;
+
+	for(i = 0; i < width; i++)
+	{
+		for(j = 0; j < height; j++)
+		{
+			Display_drawPixel(display, x + i, y + j, display->currentDrawColor);
+		}
+	}
 }
+
+
 
 void Display_drawXBM(struct DisplayGL *display, uint8_t xbmStartx, uint8_t xbmStarty, uint8_t xbmWidth, uint8_t xbmHeight, uint8_t xbm[])
 {
@@ -451,11 +487,7 @@ void Display_drawXBM(struct DisplayGL *display, uint8_t xbmStartx, uint8_t xbmSt
 			if(k == 8) k = 0;
 			if(xbm[(j / 8) + (i * xbmArrayLength)] & (1 << k))
 			{
-				Display_drawPixel(display, xbmStartx + j, xbmStarty + i);
-			}
-			else if(display->xbmMode == XBM_MODE_OVERRIDE)
-			{
-				Display_clearPixel(display, xbmStartx + j, xbmStarty + i);
+				Display_drawPixel(display, xbmStartx + j, xbmStarty + i, display->currentDrawColor);
 			}
 			k++;
 		}
--- display.h
+++ display.h
@@ -10,44 +10,30 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
-#include "STMdevice.h"	/*include perephirial library that allows interface with Spi, i2c and Gpio*/
+#if defined(__cplusplus)
+extern "C" {
+#endif
 
-#include "stdarg.h"		/*include C standart lib*/
+#include "displayConfig.h"
+
+#include "STMdevice.h"	/*include peripheral library that allows interface with Spi, i2c and Gpio*/
+
+#include "stdarg.h"		/*include C standard lib*/
 #include "stdlib.h"
-#include "stdio.h"
 
-#define DISPLAY_HEIGHT 64	/*display height in pixels*/
-#define DISPLAY_WIDTH  128	/*displal width in pixels*/
 
-#define DISPLAY_BUFFER_SIZE 1024 /*size of display buffer*/
-
-/*
- * Select communication interface for display:
- *
- * HW_SPI - use hardware spi
- * I2C - use hardware i2c
- * SW_SPI - emulate spi using gpio
- */
-
-#define DISPLAY_USE_SPI
-//#define DISPLAY_USE_I2C
-//#define DISPLAY_USE_SW_SPI
-
-typedef enum
-{
-	COLOR_BLACK = 0,
-	COLOR_WHITE
-}DisplayColor_e;
-
-typedef enum
-{
-	XBM_MODE_OVERRIDE = 0,
-	XBM_MODE_ADD
-}DisplayXbmMode_e;
+#define COLOR_BLACK		(uint16_t)0x0000	/* Most used RGB colors */
+#define COLOR_RED		(uint16_t)0xF800
+#define COLOR_GREEN		(uint16_t)0x07E0
+#define COLOR_BLUE		(uint16_t)0x001F
+#define COLOR_YELLOW	(uint16_t)0xFFE0
+#define COLOR_CYAN		(uint16_t)0x07FF
+#define COLOR_MAGENTA	(uint16_t)0xF81F
+#define COLOR_BROWN 	(uint16_t)0x9260
+#define COLOR_WHITE		(uint16_t)0xFFFF
 
 /*
- * @struct DisplayGL
- * Struct that contains information about display.
+ * @brief Struct that contains information about display
  */
 struct DisplayGL
 {
@@ -55,8 +41,8 @@ struct DisplayGL
 	uint8_t dcPin; 	/*Data/Command select pin. Set low to write command, high to write data*/
 	uint8_t resPin;	/*Reset pin. Set low to reset display*/
 
-	uint8_t dataPin; 	/*Data pin. Used as MOSI in spi mode, used as SDA in I2C mode*/
-	uint8_t clkPin;		/*Clk pin. Used as SCK in spi mode, used as SCL in I2C mode*/
+	uint8_t dataPin; 	/*Data pin. Used as MOSI in SPI mode, used as SDA in I2C mode*/
+	uint8_t clkPin;		/*Clock pin. Used as SCK in SPI mode, used as SCL in I2C mode*/
 
 	GPIO_TypeDef *csPinPort; /*Ports that contains CS, DC, RES pins*/
 	GPIO_TypeDef *dcPinPort;
@@ -65,54 +51,54 @@ struct DisplayGL
 	GPIO_TypeDef *dataPinPort; /*Ports that contains DATA and CLK pins*/
 	GPIO_TypeDef *clkPinPort;
 
-#if defined(DISPLAY_USE_SPI)
-	SPI_TypeDef *spi;	/*Spi unit that used in Hardware Spi Mode*/
-#endif
+#if defined(DISPLAY_USE_HW_4SPI)
 
-	uint8_t displayBuffer[DISPLAY_BUFFER_SIZE]; /*Array that contains display page*/
+	SPI_TypeDef *spi;	/*SPI unit that used in Hardware SPI Mode*/
 
-	DisplayColor_e currentDrawColor;
+#endif	/* display use SPI */
 
-	int8_t cursorStr;
-	int8_t cursorCol;
+	uint16_t currentDrawColor;
+	uint16_t currentBackColor;
 
-	DisplayXbmMode_e xbmMode; /*XBM draw mode. Override mode*/
+	uint8_t drawMode;
+
+	int16_t cursorX;	/* Cursor position. Used  */
+	int16_t cursorY;
 
 };
 
 void Display_init(struct DisplayGL *display);
 
-void Display_command(struct DisplayGL *display, uint8_t command);
-void Display_data(struct DisplayGL *display, uint8_t data);
-
-void Display_sendBuffer(struct DisplayGL *display);
-void Display_clearBuffer(struct DisplayGL *display);
 void Display_clear(struct DisplayGL *display);
 
-void Display_setDrawColor(struct DisplayGL *display, DisplayColor_e color);
+void Display_setDrawColor(struct DisplayGL *display, uint16_t color);
+void Display_setBackColor(struct DisplayGL *display, uint16_t color);
+void Display_setDrawMode(struct DisplayGL *display, uint8_t mode);
 
-void Display_drawPixel(struct DisplayGL *display, uint8_t x, uint8_t y);
+void Display_drawPixel(struct DisplayGL *display, uint8_t x, uint8_t y, uint16_t color);
 void Display_clearPixel(struct DisplayGL *display, uint8_t x, uint8_t y);
-void Display_fill(struct DisplayGL *display);
+void Display_fill( struct DisplayGL * display, uint16_t color );
 void Display_invert(struct DisplayGL *display);
 
 void Display_drawAsciiChar(struct DisplayGL *display, uint8_t str, uint8_t col, uint8_t asciiChr);
 void Display_printNum(struct DisplayGL *display, int32_t num);
 void Display_setCursor(struct DisplayGL *display, uint8_t str, uint8_t col);
 void Display_home(struct DisplayGL *display);
-void Display_printString(struct DisplayGL *display, uint8_t str[]);
-void Display_clearString(struct DisplayGL *display, uint8_t str);
+void Display_printString(struct DisplayGL *display, char str[]);
+
+void Display_drawFrame(struct DisplayGL *display, uint8_t x, uint8_t y, uint8_t width, uint8_t height);
+void Display_drawBox(struct DisplayGL *display, uint8_t x, uint8_t y, uint8_t width, uint8_t height);
 
 void Display_drawXBM(struct DisplayGL *display, uint8_t xbmStartx, uint8_t xbmStarty, uint8_t xbmWidth, uint8_t xbmHeight, uint8_t xbm[]);
-void Display_drawXBM8(struct DisplayGL *display, uint8_t xbmStartx, uint8_t xbmStarty, uint8_t xbmWidth, uint8_t xbmHeight, uint8_t xbm[]);
-void Display_setXbmMode(struct DisplayGL *display, DisplayXbmMode_e mode);
 
-void Display_print(struct DisplayGL *display, const char *format, ...);
+void Display_print(struct DisplayGL *display, const char *format, ...); //TODO
 
-
-//delete this shit
 #if defined(DISPLAY_USE_SW_SPI)
 void swSpiWrite(struct DisplayGL * display, uint8_t data);
+#endif
+
+#if defined(__cplusplus)
+}
 #endif
 
 #endif
